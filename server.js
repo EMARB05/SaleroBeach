9cf1aee01119beb481eea60cbfb2b371ae029749
emanuel <a24emanuelrb@iessanclemente.net>
2026-04-10 16:44:10 +0200

    arreglado tiempo de espera

--- server.js
+++ server.js
@@ -407,9 +407,10 @@ app.patch('/api/pedidos/:id/asignar', async (req, res) => {
 
 // --- INICIO DEL SERVIDOR ---
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(` Salero Bar funcionando en el puerto: ${PORT}`);
+const server = app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Salero Bar funcionando en el puerto: ${PORT}`);
 });
-// La doc sugiere aumentar estos valores para evitar "Connection reset"
+
+
 server.keepAliveTimeout = 120000;
 server.headersTimeout = 125000;
